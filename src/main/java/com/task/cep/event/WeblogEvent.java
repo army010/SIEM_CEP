ff4a0cf97c8c45706d92644bfa36c8078044f6de
Pooja Patel <pooja.patel@betitbest.com>
2019-02-10 18:02:28 +0100

    removed unnecessary code

--- src/main/java/com/task/cep/event/WeblogEvent.java
+++ src/main/java/com/task/cep/event/WeblogEvent.java
@@ -3,12 +3,6 @@ import com.task.cep.event.BaseLogEvent;
 import java.util.Objects;
 
 public class WeblogEvent extends BaseLogEvent {
-    public WeblogEvent(String time, String scanner, String type, String object, String threat, String action, String user, String information, String hash, String ipaddress) {
-        super(time, scanner, type, object, threat, action, user, information, hash, ipaddress);
-    }
-
-    public WeblogEvent() {
-    }
 
     public String toString() {
         return "WeblogEvent{" +
